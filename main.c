ab771324666f69b26150a7e4134f7f61076a0674
AugustinBallot-Ragaru <augbragaru@gmail.com>
2015-01-21 12:11:44 +0100

    Premier truc

---
+++ main.c
@@ -0,0 +1,12 @@
+double OperandeA;
+double OperandeB;
+double resultat;
+
+OperandeA = 14;
+OperandeB = 7;
+
+resultat = OperandeA * OperandeB;
+return resultat;
+
+resultat = OperandeA / OperandeB;
+return resultat;
